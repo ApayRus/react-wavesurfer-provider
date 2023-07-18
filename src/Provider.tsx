00d3a98b7a3a35becaa3519f000596e1d6337860
aparus <rustamapaev@gmail.com>
2023-07-18 10:08:18 +0400

    patch

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -217,11 +217,7 @@ export const PlayerProvider: React.FC<Props> = ({
   };
 
   const playDictationPhraseOnce = (phraseId: string) => {
-    console.log('phraseId');
-    console.log(phraseId);
     if (wavesurferRef.current) {
-      console.log('wavesurferRef.current');
-      console.log(wavesurferRef.current);
       wavesurferRef.current.regions.list[phraseId].play();
     }
   };
@@ -299,7 +295,10 @@ export const PlayerProvider: React.FC<Props> = ({
           delayMeasure,
         });
       }
-      if (phraseNum === phrases.length - 1) {
+      if (
+        phraseNum === phrases.length - 1 &&
+        currentRepeatNum === repeatCount
+      ) {
         stopDictation();
         updateState({ isFinished: true });
       }
